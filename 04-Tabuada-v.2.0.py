dfda79a55d77bf0d454ebe5d135549df16c876c9
DanielSR1 <danielsrcontato@gmail.com>
2022-03-02 22:20:01 -0300

    tabuada v2

--- 04-Tabuada-v.2.0.py
+++ 04-Tabuada-v.2.0.py
@@ -1,3 +1,4 @@
+#NESSE EXERCICIOS DESENVOLVI UM PROGRAMA CAPAZ DE DIZER QUAL A TABUDADE DE 1 A 10 DO NÚMERO ESOLHIDO.
 nu=int(input('Digite um número para ver a tabuada: '))
 for v in range(1, 11):
     r=nu*v
