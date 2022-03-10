2033ad595ffd0bf761667684b9c913c8edeecb1e
DanielSR1 <danielsrcontato@gmail.com>
2022-03-10 13:28:13 -0300

    palindromo

--- 08-palindromo.py
+++ 08-palindromo.py
@@ -1,3 +1,4 @@
+#NESSE EXERCICIO DESENVOLVI UM PROGRAMA CAPAZ DE DIZER SE UMA FRASE É UM PALINDROMO OU NÃO.
 from operator import inv
 frase=str(input('Digite a frase: ')). strip().upper()
 palavra=frase.split()
